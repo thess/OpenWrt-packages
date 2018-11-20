5ff42b0d18f68c82b28c630c93113538dd85e106
Ted Hess <thess@kitschensync.net>
2018-11-20 16:42:02 -0500

    Env vars

--- .circleci/get_commit_range.sh
+++ .circleci/get_commit_range.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
-# Check CIRCLE_COMPARE_URL first and if its not set, check for diff with master.
+# Check CIRCLE_COMPARE_URL first and if its not set, check for diff with target branch.
 
 source $BASH_ENV
-
-echo_blue "checking CIRCLE_COMPARE_URL":
-echo_blue "=== '$CIRCLE_COMPARE_URL'"
 
 if [[ ! -z "$CIRCLE_COMPARE_URL" ]]; then
     # CIRCLE_COMPARE_URL is not empty, use it to get the diff
@@ -16,15 +13,14 @@ if [[ ! -z "$CIRCLE_COMPARE_URL" ]]; then
     echo_blue "Diff: $COMMIT_RANGE"
     changes="$(git diff $COMMIT_RANGE --name-only)"
 else
-    # CIRCLE_COMPARE_URL is not set, diff with origin/HEAD
-    COMMIT_RANGE=$CIRCLE_SHA1..$CIRCLE_SHA1
-    echo_blue "Diff: $CIRCLE_SHA1"
-    changes="$(git diff-tree --no-commit-id --name-only -r $CIRCLE_SHA1)"
+    # CIRCLE_COMPARE_URL is not set, diff with $BRANCH/HEAD
+    COMMIT_RANGE="$BRANCH..$CIRCLE_SHA1"
+    echo_blue "Diff: $COMMIT_RANGE"
+    changes="$(git diff-tree --no-commit-id --name-only -r $COMMIT_RANGE)"
 fi
 
 echo_blue "Changes in this build:"
 echo_blue $changes
 echo
 # Return commit range
-echo 'export COMMIT_RANGE=$COMMIT_RANGE' >> $BASH_ENV
-
+echo export COMMIT_RANGE='$COMMIT_RANGE' >> $BASH_ENV
